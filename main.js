b3671bcb1658f906f987851d826de1788a5eeb2d
MSociopath <112525592+SerigneMourtallaSyll@users.noreply.github.com>
2023-05-26 14:37:14 +0000

    Manipulation API avec le ID

---
+++ main.js
@@ -0,0 +1,19 @@
+function searchTitle(e){
+    let url = 'https://jsonplaceholder.typicode.com/todos/';
+
+    fetch(url).then((response) => 
+        response.json().then((data) => {
+            let inputVal = document.getElementById('cp').value;
+            let songFilter = data.filter((song) => {
+                return song.id == inputVal;
+            }) 
+            document.getElementById('title').innerHTML =  `<div class="h4 pt-5"> Tile song: ${songFilter[0].title}</div>`;
+            // console.log(songFilter[0].title)
+    })
+    )
+    e.preventDefault();
+}
+// searchTitle();
+
+document.querySelector('#btn').addEventListener('click', searchTitle);
+
